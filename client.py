0441bcef324cc174d61feeb237fcf52e153c686f
Rupin <rupinmalik15@gmail.com>
2022-09-15 09:19:22 +0530

    working client server

--- client.py
+++ client.py
@@ -1,5 +1,39 @@
 import xmlrpc.client
 proxy = xmlrpc.client.ServerProxy("http://localhost:8000/") 
-print(proxy.getMoviesInfo("3","F&F"))
-
-
+if __name__ == '__main__':
+    while True:
+        
+        print(
+'''
+1. Films in theater 
+2. Get Film Location By Time
+3. Exit
+4. Book a seat
+'''
+        )
+        print("Enter your choice: ", end='')
+        choice = int(input())
+        if choice == 1:
+            Films=(proxy.view_Films())
+            for Film in Films:
+                for Film_id in Film:
+                    print("{:<15}".format(Film_id),end ="\t")
+                print("\n")
+        elif choice == 2:
+            name = input('Enter Film ID to rent your Film:').strip()
+            timings = input('Enter the time of Film').strip()
+            Filmlist=(proxy.getFilmsInfo(timings,name))
+            if(len(Filmlist)==0):
+                print("No Films available at this time")
+                continue
+            print("The Film is available in: ") 
+            for Film in Filmlist:
+                print(Film)
+        elif choice == 3:
+            print('Thank you! Visit us again!')
+            break
+        elif choice == 4:
+            name = input('Enter Film ID to rent your Film:').strip()
+            proxy.book_seat(name)
+        else:
+            print("Wrong choice! Enter correct choice")
--- server.py
+++ server.py
@@ -1,36 +1,110 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import string
+import random
+FilmInfo = [
+  {
+        "name": "F&F",
+        "time": "3",
+        "location": "Malad"
+    },{
+        "name": "Race",
+        "time": "6",
+        "location": "Malad"
+    }, {
+        "name": "Inception",
+        "time": "9",
+        "location": "Malad"
+    }
+]
 
-movieInfo = {
-  "Malad" : {
-    "name" : "F&F",
-    "time" : "3",
-    "location":"Malad"
-  },
-  "Andheri" : {
-    "name" : "Race",
-    "time" : "6",
-    "location":"Malad"
-  },
-  "Sion" : {
-    "name" : "Inception",
-    "time" : "9",
-    "location":"Malad"
-  }
-}
 
-def getMovieInfo(time,name):
-    # for x in movieInfo: 
-    #     print(x)
-    x = movieInfo.values()
-    for inforMovie in x: 
-        if inforMovie["name"] == name and inforMovie["time"] == time:
-          print(inforMovie)
-          return inforMovie
-    return "No Movie Available"
+def getFilmInfo(time, name):
+    Filmlist=[]
+    for inforFilm in FilmInfo:
+        if inforFilm["name"] == name and inforFilm["time"] == time:
+            Filmlist.append(inforFilm['location'])
+    return Filmlist
 
+
+class Film:
+ def __init__(self, Film_id, Film_name, available_seats, genre):
+    self.Film_id = Film_id
+    self.Film_name = Film_name
+    self.available_seats = available_seats
+    self.genre = genre
+ def reduceAMovieCopy(self):
+    if self.available_seats != 0:
+        self.available_seats -= 1
+    else:
+        self.available_seats = -1
+
+
+
+# Data
+Films_list = [
+    {
+        'name': 'Fast and Furious',
+        'genre': 'Thriller'
+    },
+    {
+        'name': 'Hulk',
+        'genre': 'Fiction'
+    },
+    {
+        'name': 'Avengers: End Game',
+        'genre': 'Fantasy'
+    },
+    {
+        'name': 'Lord of the Rings',
+        'genre': 'Adventure'
+    },
+    {
+        'name': 'Brahmastra',
+        'genre': 'Bollywood'
+    },
+    {
+        'name': "The Ring",
+        'genre': 'Horror'
+    }
+]
+
+
+Films = []
+for film in Films_list:
+    Films.append(
+        Film(
+            Film_id=''.join(random.choices(string.digits, k=6)), Film_name=film["name"],
+            available_seats=100,
+            genre=film["genre"]
+        )
+    )
+
+
+def view_Films():
+    table = [
+        ["Film ID", "Genre","Seats","Film Name"]]
+    # table.title = 'Films'
+    for Film in Films:
+        table.append(
+            [ 
+                Film.Film_id,Film.genre,Film.available_seats, Film.Film_name,
+                
+            ]
+        )
+    return table
+
+def book_seat(name):
+    for film in Films:
+        if(film.Film_name == name):
+            film.reduceAMovieCopy()
+            break
+    print("out")
+    return True
+    
 server = SimpleXMLRPCServer(("localhost", 8000), logRequests=True)
-server.register_function(getMovieInfo, "getMoviesInfo")
-
+server.register_function(getFilmInfo, "getFilmsInfo")
+server.register_function(view_Films,"view_Films")
+server.register_function(book_seat,"book_seat")
 try:
     print("Starting and listening on port 8000...")
     print("Press Ctrl + C to exit.")
@@ -38,4 +112,3 @@ try:
 
 except:
     print("Exit.")
-
