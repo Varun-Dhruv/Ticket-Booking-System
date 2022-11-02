090c0019937b985c5926593797df2d0c33fe6a32
Rupin <rupinmalik15@gmail.com>
2022-11-02 22:27:54 +0530

    integrated working|| mutual exclusion

--- MutualExclusion/node.py
+++ MutualExclusion/node.py
@@ -56,27 +56,31 @@ class RPCServer:
                 self.execute(after_replay=True)
 
     def execute(self, after_replay=False):
+       
         if not after_replay:
             for rpid, node in self.pid_node_mapping.items():
                 if rpid == self.pid:
                     continue
-                timestamp = time()
-                node.request(timestamp, self.pid)
-                fileread = open("myfile.txt", "r")
-                #filewrite = open("myfile.txt", "w")
-                # file read
-                num = int(fileread.read(1))
-                print(num)
-                if num == 0:
-                    print('No more seats avaible please try another time')
-                else:
-                    num = num-1
-                    print(f'one ticket booked: tickets available are: {num}')
-                    fileread.close()
-                    filewrite = open("myfile.txt", "w")
-                    filewrite.write(str(num))
-                    filewrite.close()
-                     # write file
+                try:
+                    timestamp = time()
+                
+                    node.request(timestamp, self.pid)
+                    fileread = open("myfile.txt", "r")
+                    #filewrite = open("myfile.txt", "w")
+                    # file read
+                    num = int(fileread.read(1))
+                    print(num)
+                    if num == 0:
+                        print('No more seats avaible please try another time')
+                    else:
+                        num = num-1
+                        print(f'one ticket booked: tickets available are: {num}')
+                        fileread.close()
+                        filewrite = open("myfile.txt", "w")
+                        filewrite.write(str(num))
+                        filewrite.close()
+                except Exception as e:
+                     print('')     # write file
 
 
         if set(self.pid_node_mapping.keys()) == self.reply_set:
@@ -90,27 +94,30 @@ class RPCServer:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "pid", type=str, help="Enter pid (node_1, node_2, node_3,node_4)")
-    parser.add_argument("--time_offset", type=int, default=None,
-                        help="time in seconds after which task starts to execute",)
-    args = parser.parse_args()
-    PID = args.pid
-    TIME_OFFSET = args.time_offset
-    server = SimpleXMLRPCServer(
-        NETWORK[PID], allow_none=True, logRequests=False)
-    pid_mapping = dict()
-    for pid, addr in NETWORK.items():
-        if pid == PID:
-            continue
-        pid_mapping[pid] = ServerProxy(f"http://{addr[0]}:{addr[1]}")
-    crs_proxy = ServerProxy(f"http://{CRS[0]}:{CRS[1]}")
-    server.register_instance(
-        RPCServer(PID, pid_mapping, crs_proxy, TIME_OFFSET))
-    try:
-        print("Starting RPC Server...")
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print("\nKeyboard interrupt received, exiting.")
-        sys.exit(0)
+    try: 
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "pid", type=str, help="Enter pid (node_1, node_2, node_3,node_4)")
+        parser.add_argument("--time_offset", type=int, default=None,
+                            help="time in seconds after which task starts to execute",)
+        args = parser.parse_args()
+        PID = args.pid
+        TIME_OFFSET = args.time_offset
+        server = SimpleXMLRPCServer(
+            NETWORK[PID], allow_none=True, logRequests=False)
+        pid_mapping = dict()
+        for pid, addr in NETWORK.items():
+            if pid == PID:
+                continue
+            pid_mapping[pid] = ServerProxy(f"http://{addr[0]}:{addr[1]}")
+        crs_proxy = ServerProxy(f"http://{CRS[0]}:{CRS[1]}")
+        server.register_instance(
+            RPCServer(PID, pid_mapping, crs_proxy, TIME_OFFSET))
+        try:
+            print("Starting RPC Server...")
+            server.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
+    except Exception as e:
+        print('')
